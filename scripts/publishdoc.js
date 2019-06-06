e7901076bcba372e26ee374aa3bcea2bb33530d6
liuyuan <939692178@qq.com>
2019-06-06 17:24:39 +0800

    feat:add gh-pages

--- scripts/publishdoc.js
+++ scripts/publishdoc.js
@@ -1,7 +1,7 @@
 var ghpages = require('gh-pages');
 
 ghpages.publish('docsDist', {
-  repo: 'git@github.com:ming-cult/snake-design.git'
+  repo: 'git@github.com:liuyuan6088/naruto-ui.git'
 }, (err) => {
   if (err) {
     console.log(err)
